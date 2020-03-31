aa676de6e4b9565000e7b7abb6185c929759aa45
Marx.W <boyuan@vt.edu>
2020-03-30 20:05:05 -0400

    step 7

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,10 +1,22 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 import './body.html';
+import './task.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+  });
 
 Template.body.helpers({
     tasks() {
+        const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+
         // Show newest tasks at the top
         return Tasks.find({}, { sort: { createdAt: -1 } });
       },
@@ -28,5 +40,11 @@ Template.body.events({
    
       // Clear form
       target.text.value = '';
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+      },
+    incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
     },
   });
